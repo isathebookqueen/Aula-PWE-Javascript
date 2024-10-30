be0ff263951682f085272a064bad9aed1429fd2e
Isadora Cavalcante <107890493+isathebookqueen@users.noreply.github.com>
2024-10-30 11:36:16 +0000

    commit3

---
+++ questao3.js
@@ -0,0 +1,8 @@
+var a="João da Silva";
+var b=7;
+var c=8.5;
+var d=b*0.6;
+var e=c*0.4;
+var f=d+e;
+alert("Nome do aluno: "+a+" \nNota 1: "+b+" \nNota 2: "+c);
+alert("Nome do aluno: "+a+"\nNota final: "+f);
